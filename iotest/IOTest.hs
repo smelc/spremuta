a7b609f15dcc801c6c2f4b827264889886e86735
Clément Hurlin <clement.hurlin@gmail.com>
2023-12-21 17:13:30 +0100

    Add IO tests

--- iotest/IOTest.hs
+++ iotest/IOTest.hs
@@ -2,6 +2,30 @@ module Main where -- !!!module name different from filename!!! (for 'cabal test'
 
 import           System.Exit
 
+import           Data.List
+import           Data.String.Utils
+import           System.Process.Extra (readProcessWithExitCode)
+import           Test.Hspec
+
+assertSpremutaOK :: [String] -> Expectation
+assertSpremutaOK callArgs = do
+  (ec, stdout, stderr) <- readProcessWithExitCode program args ""
+  case ec of
+    ExitSuccess -> pure ()
+    ExitFailure _ -> do
+      logHandle "✅ stdout" $ strip stdout
+      logHandle "❌ stderr" $ strip stderr
+      expectationFailure $ "🙁 Success was expected, but command failed with exit code " ++ show ec ++ ":\n" ++ intercalate " " args
+  where
+    program = "cabal"
+    args = ["run", "spremuta", "--"] ++ callArgs
+    logHandle name content =
+      putStrLn $ case content of
+        "" -> "No " ++ name ++ " ¯ \\ _ (ツ) _ / ¯"
+        _  -> name ++ ": " ++ content
+
 main :: IO ()
-main = do
-  exitSuccess
+main = hspec $ do
+  describe "test" $ do
+    it "task" $
+      assertSpremutaOK ["task", "notify when https://github.com/input-output-hk/cardano-cli/pull/513 ismerged"]
